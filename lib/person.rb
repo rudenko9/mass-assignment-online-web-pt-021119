a500eb8777235265035e8f145dde0482053a1326
Nataliya R <n.rudenko@gmail.com>
2019-03-24 21:14:25 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -3,8 +3,8 @@ attr_accessor :name, :birthday, :hair_color, :eye_color, :height,
 :weight, :handed, :complexion, :t_shirt_size, 
 :wrist_size, :glove_size, :pant_length, :pant_widt
 
-def initialize(name_atributes)
-  name_atributes.each {|key, value| self.send(("#{key}="), value)}
+def initialize(atributes)
+  atributes.each {|key, value| self.send(("#{key}="), value)}
 end 
 
 
